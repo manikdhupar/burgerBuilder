f78481740d4c6161607d73a932bc9a281785a015
manik dhupar <manik.dhupar7@gmail.com>
2018-07-14 23:37:07 +0530

    dynamic BurgerIngridient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,9 +3,24 @@ import BurgerIngridients from './BurgerIngridients/BurgerIngridients';
 import classes from './Burger.css';
 
 const burger = (props) => {
+	let transformedIngridients = Object.keys(props.ingridients)
+		.map((igKey) => {
+			return [ ...Array(props.ingridients[igKey]) ].map((_, index) => {
+				return <BurgerIngridients key={igKey + index} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (transformedIngridients.length === 0) {
+		transformedIngridients = <div>You need to add ingidients!</div>;
+	}
+
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngridients type="bread-top" />
+			{transformedIngridients}
 			<BurgerIngridients type="bread-bottom" />
 		</div>
 	);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+	state = {
+		ingridients: {
+			salad: 0,
+			cheese: 0,
+			bacon: 0,
+			meat: 0
+		}
+	};
 	render() {
 		return (
 			<React.Fragment>
-				<Burger />
+				<Burger ingridients={this.state.ingridients} />
 				<div>Build Controls</div>
 			</React.Fragment>
 		);
