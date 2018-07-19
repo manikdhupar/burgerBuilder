8b562f4608e0d5f05eaa0df6279e7a933ca841d9
manik dhupar <manik.dhupar7@gmail.com>
2018-07-19 13:30:03 +0530

    added Toolbar component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => {
 	return (
 		<React.Fragment>
 			<div>Toolbar,Sidedrawer,Backdrop</div>
+			<Toolbar />
 			<main className={classes.Content}>{props.children}</main>
 		</React.Fragment>
 	);
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Toolbar.css';
+
+const toolbar = (props) => (
+	<header className={classes.Toolbar}>
+		<div>Menu</div>
+		<div>Logo</div>
+		<nav>
+			<ul>...</ul>
+		</nav>
+	</header>
+);
+
+export default toolbar;
