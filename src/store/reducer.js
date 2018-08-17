4f1096a032200e07b9171973ec5571aa2124dfdc
manik dhupar <manik.dhupar7@gmail.com>
2018-08-17 21:23:40 +0530

    added redux setup

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
+import * as actionTypes from './actions';
+
 const initialState = {
 	ingredients: null,
 	totalPrice: 0
 };
-import * as actionTypes from './actions';
+
 const reducer = (state = initialState, action) => {};
 
 export default reducer;
