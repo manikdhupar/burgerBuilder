2198b446f4392fc02de37b4d777a27ea981e3928
manik dhupar <manik.dhupar7@gmail.com>
2018-08-17 21:14:05 +0530

    added redux basic setup

---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+	ingredients: null,
+	totalPrice: 0
+};
+import * as actionTypes from './actions';
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
